a3548b4ce403c877f077f59695919e5e62b7b22d
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-03-06 22:25:44 +0000

    added IconWrapper

--- src/Files/Components/Icons.js
+++ src/Files/Components/Icons.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { IconWrapper } from "../../Styles";
 
 let width = 30;
 let height = 30;
@@ -30,7 +31,7 @@ export const BackIcon = () => {
 
 export const CartIcon = () => {
   return (
-    <div className="icon-wrapper">
+    <IconWrapper>
       <svg
         viewBox="0 0 16 16"
         className="bi bi-cart3 icon"
@@ -44,13 +45,13 @@ export const CartIcon = () => {
           d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"
         />
       </svg>
-    </div>
+    </IconWrapper>
   );
 };
 
 export const JustifyIcon = () => {
   return (
-    <div className="icon-wrapper">
+    <IconWrapper>
       <svg
         viewBox="0 0 16 16"
         className="bi bi-justify icon"
@@ -64,13 +65,13 @@ export const JustifyIcon = () => {
           d="M2 12.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"
         />
       </svg>
-    </div>
+    </IconWrapper>
   );
 };
 
 export const UserIcon = () => {
   return (
-    <div className="icon-wrapper">
+    <IconWrapper>
       <svg
         viewBox="0 0 16 16"
         className="bi bi-person-circle icon"
@@ -86,13 +87,13 @@ export const UserIcon = () => {
           d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z"
         />
       </svg>
-    </div>
+    </IconWrapper>
   );
 };
 
 export const SearchIcon = () => {
   return (
-    <div className="icon-wrapper">
+    <IconWrapper>
       <svg
         viewBox="0 0 16 16"
         className="bi bi-search icon"
@@ -110,7 +111,7 @@ export const SearchIcon = () => {
           d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"
         />
       </svg>
-    </div>
+    </IconWrapper>
   );
 };
 
